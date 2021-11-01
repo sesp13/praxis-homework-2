24765bec1099f8538cca86fec9bd7f5da95a0b62
sesp13 <sespinosarte99@gmail.com>
2021-11-01 01:49:45 -0500

    install go and vagrant

--- build-app/bootstrap.sh
+++ build-app/bootstrap.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
-echo "Hello world!"
+# Install go
+sudo yum install git -y
+
+# Install golang
+sudo yum install golang -y 
+
+# Install node js
+
+# Git version
+git --version
+
+# Echo go version
+go version
